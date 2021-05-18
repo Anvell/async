8f1b31e3a0f4510ef6917749bc2e7defbda98067
Lukasz Wolanczyk <lukasz.wolanczyk@ergo.com>
2021-05-18 13:00:07 +0200

    Setup unit tests

---
+++ async/src/test/kotlin/io/github/anvell/async/AsyncTest.kt
@@ -0,0 +1,37 @@
+package io.github.anvell.async
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class AsyncTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    internal fun example1() {
+        true shouldBe true
+    }
+
+    @Test
+    internal fun example2() {
+        true shouldBe true
+    }
+
+    @Test
+    internal fun example3() {
+        true shouldBe true
+    }
+
+    @Test
+    internal fun example4() {
+        true shouldBe true
+    }
+}
