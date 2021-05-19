5f6a09fb6ca6c5d07a1b30ff7d06e4a1ced23d73
Lukasz Wolanczyk <lukasz.wolanczyk@ergo.com>
2021-05-19 14:24:13 +0200

    Implement unit tests for Async

--- async/src/test/kotlin/io/github/anvell/async/AsyncTest.kt
+++ async/src/test/kotlin/io/github/anvell/async/AsyncTest.kt
@@ -1,37 +1,169 @@
 package io.github.anvell.async
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import java.lang.NullPointerException
 
-internal class AsyncTest {
+internal class AsyncTest: DescribeSpec({
+    lateinit var async: Async<String>
 
-    @BeforeEach
-    fun setUp() {
+    it("should have only four possible states") {
+        Async::class.sealedSubclasses.size shouldBe 4
+        Async::class.sealedSubclasses shouldContainAll listOf(
+            Uninitialized::class,
+            Loading::class,
+            Success::class,
+            Fail::class
+        )
     }
 
-    @AfterEach
-    fun tearDown() {
+    describe("invoking") {
+        context("when value is uninitialized") {
+            beforeEach {
+                async = Uninitialized
+            }
+
+            it("should not return any value") {
+                async() shouldBe null
+            }
+        }
+
+        context("when value is loading") {
+            beforeEach {
+                async = Loading
+            }
+
+            it("should not return any value") {
+                async() shouldBe null
+            }
+        }
+
+        context("when value has been successfully loaded") {
+            beforeEach {
+                async = Success("FooBar")
+            }
+
+            it("should return actual value") {
+                async() shouldBe "FooBar"
+            }
+
+            it("should consume value") {
+                async()
+                (async as Success).isConsumed shouldBe true
+            }
+        }
+
+        context("when loading value has failed") {
+            beforeEach {
+                async = Fail(Throwable())
+            }
+
+            it("should not return any value") {
+                async() shouldBe null
+            }
+
+            it("should not consume error") {
+                async()
+                (async as Fail).isConsumed shouldBe false
+            }
+        }
     }
 
-    @Test
-    internal fun example1() {
-        true shouldBe true
+    describe("unwrapping") {
+        context("when value is uninitialized") {
+            beforeEach {
+                async = Uninitialized
+            }
+
+            it("should throw IllegalArgumentException") {
+                shouldThrow<IllegalArgumentException> { async.unwrap() }
+            }
+        }
+
+        context("when value is loading") {
+            beforeEach {
+                async = Loading
+            }
+
+            it("should throw IllegalArgumentException") {
+                shouldThrow<IllegalArgumentException> { async.unwrap() }
+            }
+        }
+
+        context("when value has been successfully loaded") {
+            beforeEach {
+                async = Success("FooBar")
+            }
+
+            it("should return actual value") {
+                async.unwrap() shouldBe "FooBar"
+            }
+
+            it("should consume value") {
+                async.unwrap()
+                (async as Success).isConsumed shouldBe true
+            }
+        }
+
+        context("when loading value has failed") {
+            beforeEach {
+                async = Fail(Throwable())
+            }
+
+            it("should throw IllegalArgumentException") {
+                shouldThrow<IllegalArgumentException> { async.unwrap() }
+            }
+        }
+    }
+})
+
+internal class SuccessTest: DescribeSpec({
+    lateinit var success: Success<String>
+
+    beforeEach {
+        success = Success("FooBar")
     }
 
-    @Test
-    internal fun example2() {
-        true shouldBe true
+    describe("peeking a value") {
+        it("should return actual value") {
+            success.peek() shouldBe "FooBar"
+        }
+
+        it("should not consume value") {
+            success.peek()
+            success.isConsumed shouldBe false
+        }
+    }
+})
+
+internal class FailTest: DescribeSpec({
+    lateinit var fail: Fail<String>
+
+    beforeEach {
+        fail = Fail(NullPointerException())
     }
 
-    @Test
-    internal fun example3() {
-        true shouldBe true
+    describe("obtaining an error") {
+        it("should return actual error") {
+            fail.error shouldBe NullPointerException()
+        }
+
+        it("should consume error") {
+            fail.error
+            fail.isConsumed shouldBe true
+        }
     }
 
-    @Test
-    internal fun example4() {
-        true shouldBe true
+    describe("peeking an error") {
+        it("should return actual error") {
+            fail.peekError() shouldBe NullPointerException()
+        }
+
+        it("should not consume error") {
+            fail.peekError()
+            fail.isConsumed shouldBe false
+        }
     }
-}
+})
