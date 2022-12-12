2f77fb510dd8f187fcc10ff7a1dcb3ad9512189f
lukwol <wolanczyk.lukasz@gmail.com>
2022-12-12 15:53:56 +0100

    Add AsyncState documentation

--- async-state/src/commonMain/kotlin/io/github/anvell/async/state/AsyncState.kt
+++ async-state/src/commonMain/kotlin/io/github/anvell/async/state/AsyncState.kt
@@ -5,20 +5,38 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlin.reflect.KProperty1
 
+/**
+ * Set of operators for UDF state management based on [Async].
+ */
 interface AsyncState<S> {
 
+    /**
+     * Delegate implementation of [AsyncState] based on [MutableStateFlow].
+     */
     class Delegate<S>(initialState: S) : AsyncState<S> {
         override val stateFlow = MutableStateFlow(initialState)
     }
 
+    /**
+     * [MutableStateFlow] with current state.
+     */
     val stateFlow: MutableStateFlow<S>
 
+    /**
+     * Provide current state in [block]
+     */
     fun <V> withState(block: (S) -> V) = block(stateFlow.value)
 
+    /**
+     * Update current state with [reducer].
+     */
     fun setState(reducer: S.() -> S) {
         stateFlow.value = reducer(stateFlow.value)
     }
 
+    /**
+     * Subscribe for [property] changes in state.
+     */
     fun <P> CoroutineScope.selectSubscribe(
         property: KProperty1<S, P>,
         block: (P) -> Unit
@@ -29,6 +47,9 @@ interface AsyncState<S> {
             .collect(block)
     }
 
+    /**
+     * Collect flow and update state with [reducer].
+     */
     fun <V> Flow<Result<V>>.collectReduceAsState(
         scope: CoroutineScope,
         initialState: Async<V>? = Loading,
@@ -46,6 +67,9 @@ interface AsyncState<S> {
             }
     }
 
+    /**
+     * Collect flow and update state with [reducer].
+     */
     fun <V> Flow<V>.collectAsState(
         scope: CoroutineScope,
         initialState: Async<V>? = Loading,
@@ -58,6 +82,9 @@ interface AsyncState<S> {
             .collect { setState { reducer(Success(it)) } }
     }
 
+    /**
+     * Await [result][Result] and update state with [reducer].
+     */
     fun <V> ScopedDeferred<Result<V>>.reduceAsState(
         initialState: Async<V>? = Loading,
         reducer: S.(Async<V>) -> S
@@ -73,6 +100,9 @@ interface AsyncState<S> {
         }
     }
 
+    /**
+     * Await value and update state with [reducer].
+     */
     fun <V> ScopedDeferred<V>.catchAsState(
         initialState: Async<V>? = Loading,
         reducer: S.(Async<V>) -> S
